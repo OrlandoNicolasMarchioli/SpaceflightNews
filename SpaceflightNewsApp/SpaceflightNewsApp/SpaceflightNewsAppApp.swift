d5284c84c43c99ecc4788c2f419c7bdb17b402ca
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2026-05-19 00:40:46 -0300

    Convert SpaceflightNewsApp from submodule to regular folder

---
+++ SpaceflightNewsApp/SpaceflightNewsApp/SpaceflightNewsAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  SpaceflightNewsAppApp.swift
+//  SpaceflightNewsApp
+//
+//  Created by Orlando Nicola Marchioli on 17/05/2026.
+//
+
+import SwiftUI
+import SpaceflightNews
+
+@main
+struct SpaceflightNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
+
